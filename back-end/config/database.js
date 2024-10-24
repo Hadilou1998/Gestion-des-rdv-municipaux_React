7ba67c07b31967cadc775c6baf5f9d8470105dc8
Hadilou1998 <hadilissilame@gmail.com>
2024-10-24 16:02:51 +0200

    Added 2 packages, .gitignore and database.js

---
+++ back-end/config/database.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        timezone: "+01:00"
+    }
+}
