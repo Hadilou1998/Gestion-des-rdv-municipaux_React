fdb4e46d4fc8c028b987f0f6fb4af3b67352e624
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-08 17:45:26 +0100

    Updated App.js, package-lock.json and package.json

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -9,13 +9,13 @@ import Appointment from "./pages/Appointment";
 const App = () => {
   <Router>
     <NavigationBar />
-    <Switch>
+    <Routes>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
       <Route path="/appointment" component={Appointment} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/" exact component={Dashboard} />
-    </Switch>
+    </Routes>
   </Router>
 };
 
