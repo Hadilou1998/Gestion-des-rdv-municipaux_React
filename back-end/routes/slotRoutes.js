0b171492d4e68be7d8a214bf02f27bfc43454fa4
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-08 17:39:19 +0100

    Updated serviceRoutes and slotRoutes

--- back-end/routes/slotRoutes.js
+++ back-end/routes/slotRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const slotController = require('../controllers/slotController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Liste des créneaux disponibles
 router.get('/', slotController.getAllSlots);
