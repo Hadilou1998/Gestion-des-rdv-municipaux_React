d89db10f8cf0efac9c65358e305de6c5bbd53360
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-14 21:39:16 +0100

    Updated database.js, package and package-lock

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*require('dotenv').config();
 
 module.exports = {
     development: {
@@ -15,4 +15,25 @@ module.exports = {
         },
         timezone: "+01:00",
     },
-};
+};*/
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false, // Désactiver les logs SQL
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
+
+module.exports = sequelize;
