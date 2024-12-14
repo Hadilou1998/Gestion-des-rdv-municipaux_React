46a5a0bf1c2a5b0a3adab7550bfb1fa49f36ed5e
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-14 21:49:21 +0100

    Updated models Service.js

--- back-end/models/Service.js
+++ back-end/models/Service.js
@@ -1,29 +1,31 @@
-module.exports = (sequelize, DataTypes) => {
-    const Service = sequelize.define('Service', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-        },
-        duration: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        department: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        isActive: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true,
-        }
-    }, { timestamps: true, underscored: true });
-    return Service;
-};
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Service = sequelize.define('Service', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    department: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+    }
+}, { timestamps: true });
+
+module.exports = Service;
