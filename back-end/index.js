7eabc5c29fab711f0b90b7cac6af7db1c9bec448
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-14 23:09:56 +0100

    Updated index.js of back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,36 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const db = require('./models');
+
+// Charger les variables d'environnement
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Importer les routes
 const authRoutes = require('./routes/authRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 const slotRoutes = require('./routes/slotRoutes');
-const helmet = require('helmet');
-const cors = require('cors');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const Sequelize = require('sequelize');
-const sequelize = require('./models');
-
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'API de gestion de rendez-vous municipaux',
-            version: '1.0.0',
-            description: 'Documentation sur l\'API',
-        },
-    },
-    apis: ['./routes/*.js']
-}
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-// Middlewares
-app.use(express.json());
-app.use(helmet());
-app.use(cors({ origin: 'https://localhost:5000' }));
-
-// Documentation avec Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use('/api/auth', authRoutes);
@@ -38,22 +22,12 @@ app.use('/api/appointments', appointmentRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/slots', slotRoutes);
 
-// Middleware d'erreur
-app.use((req, res, next) => {
-    res.status(404).json({ error: 'Route non trouvée' });
-});
-
-// Connection à la base de données
-sequelize
-    .authenticate()
-    .then(() => console.log("Connexion à la base de données réussie"))
-    .catch(err => console.error("Erreur de connexion à la base de données:", err));
-
 // Démarrage du serveur
 const port = process.env.PORT || 5000;
 
-sequelize.sync().then(() => {
-    app.listen(port, () => {
-        console.log(`Serveur démarré sur le port ${port}`);
-    });
-});
+db.sequelize.sync({ alter: true }).then(() => {
+    console.log('Base de données synchronisée.');
+    app.listen(PORT, () => console.log(`Serveur en écoute sur le port ${PORT}`));
+  }).catch((error) => {
+    console.error('Erreur de connexion à la base de données :', error);
+  });
