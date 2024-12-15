fb8fe0e3cff4e45b081d4789c8a12ad0646a7664
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-15 22:02:42 +0100

    Updated Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Footer = () => {
-    <footer className="bg-dark text-white text-center py-3">
-        <p>&copy 2025 Mairie Municipale. Tous droits réservés.</p>
-    </footer>
+function Footer() {
+    return (
+        <footer className="bg-dark text-white text-center py-3">
+            <p>&copy 2025 Mairie Municipale. Tous droits réservés.</p>
+        </footer>
+    );
 };
 
 export default Footer;
