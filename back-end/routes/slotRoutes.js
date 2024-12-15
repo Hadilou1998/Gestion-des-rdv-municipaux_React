72afd3d97d64e85d467850a37dd490dc64e9e680
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-15 12:46:15 +0100

    Updated slottRoutes.js

--- back-end/routes/slotRoutes.js
+++ back-end/routes/slotRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const slotController = require('../controllers/slotController');
-const authMiddleware = require('../middleware/authMiddleware');
+const auth = require('../middleware/auth');
 
 // Liste des créneaux disponibles
 router.get('/', slotController.getAllSlots);
 
 // Création d'un créneau
-router.post('/', authMiddleware, slotController.createSlot);
+router.post('/', auth, slotController.createSlot);
 
 // Consultation d'un créneau par son ID
 router.get('/:id', slotController.getSlotById);
 
 // Modification d'un créneau par son ID
-router.put('/:id', authMiddleware, slotController.updateSlot);
+router.put('/:id', auth, slotController.updateSlot);
 
 // Suppression d'un créneau par son ID
-router.delete('/:id', authMiddleware, slotController.deleteSlot);
+router.delete('/:id', auth, slotController.deleteSlot);
 
 module.exports = router;
