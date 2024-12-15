500178f2c533df39c16f5673f071f733a0285154
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-15 22:03:20 +0100

    Updated Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="bg-dark text-white text-center py-3">
-            <p>&copy 2025 Mairie Municipale. Tous droits réservés.</p>
+            <p>&copy; 2025 Mairie Municipale. Tous droits réservés.</p>
         </footer>
     );
 };
