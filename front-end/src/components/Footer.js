60d9458e59c7f097297c426f933d75f2c77a3438
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-15 22:36:50 +0100

    Updated Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="bg-dark text-white text-center py-3">
-            <p>&copy; 2025 Mairie Municipale. Tous droits réservés.</p>
-        </footer>
+        <div className="d-flex flex-column min-vh-100">
+            <footer className="bg-dark text-white text-center py-3 mt-auto">
+                <p>&copy; 2025 Mairie Municipale. Tous droits réservés.</p>
+            </footer>
+        </div>
     );
 };
 
