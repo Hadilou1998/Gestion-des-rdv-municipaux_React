3f566a06b8fa0b686c3e5e20d199aea7cbfe1cc1
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-18 00:41:14 +0100

    Updated Header.js

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -7,9 +7,9 @@ function Header() {
     const { user } = useContext(UserContext); // Accès à l'utilisateur connecté
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <Link className="navbar-brand" to="/">Rendez-Vous Municipaux</Link>
+                <Link className="navbar-brand" to="/">Ville d'Argenteuil</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span className="navbar-toggler-icon"></span>
                 </button>
