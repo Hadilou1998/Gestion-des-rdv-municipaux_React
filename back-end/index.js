0099795c105731e9f0c3079d16b512733c06c63a
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-18 00:36:33 +0100

    Updated index.js

--- back-end/index.js
+++ back-end/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 // Importer les routes
 const authRoutes = require('./routes/authRoutes');
