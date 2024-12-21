684f384bc8edc25c7e275380c6abdec71685f420
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-21 18:52:59 +0100

    Updated database.js config

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,16 +1,32 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    timezone: "+01:00",
-    logging: false,
-  }
-);
-
-module.exports = sequelize;
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        timezone: "+01:00",
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        timezone: "+01:00",
+    },
+};
