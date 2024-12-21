a427c3d2b18aa7f37bad3d5d7bfbfb1cc34a7956
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-21 21:02:04 +0100

    Updated database.js

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,32 +1,18 @@
-require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-module.exports = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mysql',
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-        timezone: "+01:00",
+// Configuration de Sequelize
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql', // ou 'postgres', 'sqlite', etc.
+    logging: true, // Afficher les logs dans le console
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
     },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-        timezone: "+01:00",
-    },
-};
+    timezone: "+01:00",
+});
+
+// Exporter l'instance de Sequelize
+module.exports = sequelize;
