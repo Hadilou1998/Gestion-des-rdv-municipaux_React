8ee913a6a0ad514a0066335bd8e3b22c50c90576
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-22 23:29:59 +0100

    Updated Calendar, package and package-lock

--- front-end/src/pages/Calendar.js
+++ front-end/src/pages/Calendar.js
@@ -1,48 +1,46 @@
-import React, { useState } from 'react';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { format, parse, startOfWeek, getDay } from 'date-fns';
-import fr from 'date-fns/locale/fr';
+import React, { useState } from "react";
+import { Calendar as BigCalendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import frLocale from "date-fns/locale/fr";
 
-const locales = { fr };
+const locales = {
+  fr: frLocale,
+};
+
 const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
 });
 
 const events = [
-    {
-        title: 'Rendez-vous passeport',
-        start: new Date(2024, 0, 10, 10, 0),
-        end: new Date(2024, 0, 10, 11, 0),
-    },
-    {
-        title: 'Rendez-vous carte d’identité',
-        start: new Date(2024, 0, 15, 14, 0),
-        end: new Date(2024, 0, 15, 15, 0),
-    },
+  {
+    title: "Rendez-vous avec le maire",
+    start: new Date(2023, 11, 25, 14, 0),
+    end: new Date(2023, 11, 25, 15, 0),
+    allDay: false,
+  },
 ];
 
-function CalendarPage() {
-    const [myEvents] = useState(events);
+function Calendar() {
+  const [myEvents] = useState(events);
 
-    return (
-        <div className="container mt-5">
-            <h1>Calendrier</h1>
-            <div className="mt-4">
-                <Calendar
-                    localizer={localizer}
-                    events={myEvents}
-                    startAccessor="start"
-                    endAccessor="end"
-                    style={{ height: 500 }}
-                />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="container my-5">
+      <h2 className="text-center">Calendrier</h2>
+      <BigCalendar
+        localizer={localizer}
+        events={myEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        culture="fr"
+      />
+    </div>
+  );
+}
 
-export default CalendarPage;
+export default Calendar;
