141ff9f0138786827ec2a12997428b8d64f15b38
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-24 20:54:27 +0100

    Updated Profile.js and Logout.js

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
+import { UserContext } from "../context/UserContext";
 
 function Profile() {
     const { user } = useContext(UserContext);
