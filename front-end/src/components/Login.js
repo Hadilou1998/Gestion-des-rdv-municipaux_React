6741a9b628848254de8e4342d8649b031de7f437
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-26 00:05:35 +0100

    Updated the folder components

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../../context/UserContext";
+import { UserContext } from "../context/UserContext";
 
 function Login() {
     const [email, setEmail] = useState("");
