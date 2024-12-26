927b26bea90d002eb1db189b3eecbe242c694734
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2024-12-26 21:51:39 +0100

    Updated UserContext.js

--- front-end/src/context/UserContext.js
+++ front-end/src/context/UserContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 // Création du context utilisateur
 export const UserContext = createContext();
@@ -7,16 +8,32 @@ export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null); // Utilisateur connecté
 
+    const register = async (userData) => {
+        const response = await axios.post("/api/auth/register", userData);
+        setUser(response.data.user);
+    };
+
+    const login = async (userData) => {
+        const response = await axios.post("/api/auth/login", userData);
+        setUser(response.data.user);
+        localStorage.setItem("user", JSON.stringify(response.data.token));
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
     // Récupération des informations utilisateur depuis le localStorage
     useEffect(() => {
-        const token = localStorage.getItem("token");
+        const token = localStorage.getItem("user");
         if (token) {
-            setUser(token);
+            setUser(JSON.parse(token));
         }
     }, []);
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, register, login, logout }}>
             {children}
         </UserContext.Provider>
     );
