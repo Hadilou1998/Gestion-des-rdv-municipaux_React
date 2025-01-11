86543e254367a7beb42a5de360a7d5a8899d1faf
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2025-01-11 23:18:21 +0100

    Updated validateMiddleware.js backend

--- back-end/middleware/validateMiddleware.js
+++ back-end/middleware/validateMiddleware.js
@@ -1,7 +1,6 @@
 const { check, validationResult } = require("express-validator");
 
 exports.validationAppointment = [
-    check("user_id").isInt().withMessage("UserId must be a valid integer"),
     check("service_id").isInt().withMessage("ServiceId must be a valid integer"),
     check("appointmentDate").isISO8601().withMessage("Appointment date must be valid"),
     (req, res, next) => {
