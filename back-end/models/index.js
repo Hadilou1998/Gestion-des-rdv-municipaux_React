7fcb61ff86645b771a2a823f9e88251d621ffd04
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2025-01-12 21:52:23 +0100

    Updated model index.js

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,23 +1,36 @@
 const { Sequelize, DataTypes } = require('sequelize');
+
+// Initialisation de la connexion à la base de données
 const sequelize = new Sequelize('base_mairie', 'root', 'supersaiyan', {
     host: 'localhost',
-    dialect: 'mysql', // ou 'postgres', 'sqlite', etc.
+    dialect: 'mysql', // Changez en fonction de votre SGBD (par exemple : 'postgres', 'sqlite', etc.)
+    logging: false, // Désactivez les logs SQL pour plus de clarté, activez-les pour le debug.
 });
 
-// Définir les modèles
-const User = require('./User');
-const Service = require('./Service');
-const Appointment = require('./Appointment');
-const TimeSlot = require('./TimeSlot');
+// Importation des modèles
+const User = require('./User')(sequelize, DataTypes);
+const Service = require('./Service')(sequelize, DataTypes);
+const Appointment = require('./Appointment')(sequelize, DataTypes);
+const TimeSlot = require('./TimeSlot')(sequelize, DataTypes);
 
-// Ajout des modèles à `db`
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// Ajout des modèles dans un objet pour leur gestion centralisée
+const models = {
+    User,
+    Service,
+    Appointment,
+    TimeSlot,
+};
 
-db.User = User(sequelize, Sequelize.DataTypes);
-db.Service = Service(sequelize, Sequelize.DataTypes);
-db.Appointment = Appointment(sequelize, Sequelize.DataTypes);
-db.TimeSlot = TimeSlot(sequelize, Sequelize.DataTypes);
+// Initialisation des associations
+Object.keys(models).forEach((modelName) => {
+    if (models[modelName].associate) {
+        models[modelName].associate(models); // Appelle les méthodes d'association dans chaque modèle
+    }
+});
 
-module.exports = db;
+// Export des modèles et de la connexion Sequelize
+module.exports = {
+    sequelize,
+    Sequelize,
+    ...models,
+};
