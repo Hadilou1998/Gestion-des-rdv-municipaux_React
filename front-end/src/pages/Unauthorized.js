3ec6c9b3f24570475d14ef0cfe8b7debb9a7dd1f
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2025-01-18 00:03:01 +0100

    Created Unauthorized.js and UserContext.js

---
+++ front-end/src/pages/Unauthorized.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Unauthorized() {
+    return (
+        <div className="container mt-4">
+            <h2>Accès interdit</h2>
+            <p>Vous n'êtes pas autorisé à accéder à cette page.</p>
+        </div>
+    );
+};
+
+export default Unauthorized;
