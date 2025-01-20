e1968df764186524fbeafcbbda2648a52a80880d
Hadilou1998 <121810376+dev7socializus@users.noreply.github.com>
2025-01-20 12:23:47 +0100

    Updated Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="d-flex flex-column min-vh-100">
+        <div className="d-flex flex-column min-vh-90">
             <footer className="bg-dark text-white text-center p-3 mt-auto">
                 <p className="mb-0">&copy; 2025 Mairie Municipale. Tous droits réservés.</p>
             </footer>
         </div>
     );
-};
+}
 
 export default Footer;
