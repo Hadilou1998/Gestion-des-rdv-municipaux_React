2e141d7ca2b6e58c74d649d31a0e69ea90051e4d
Hadilou1998 <hadilissilame@gmail.com>
2025-02-06 14:20:50 +0100

    Modify import services/api in AppointmentDetails

--- front-end/src/components/Appointments/AppointmentDetails.js
+++ front-end/src/components/Appointments/AppointmentDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "../services/api";
+import axios from "../../services/api";
 
 function AppointmentDetails() {
     const { id } = useParams();
