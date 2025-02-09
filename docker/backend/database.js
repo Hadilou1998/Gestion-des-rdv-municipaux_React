4ddbc0029fbd635c0acb79849bf925e67128f938
Hadilou1998 <hadilissilame@gmail.com>
2025-02-09 12:47:07 +0100

    Created folder backend and frontend and updated folder docker

--- docker/backend/database.js
+++ docker/backend/database.js
@@ -1,5 +1,13 @@
 const { Sequelize } = require('sequelize');
 
+// Afficher les valeurs des variables d'environnement pour débogage
+console.log('Configuration de la base de données :');
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
