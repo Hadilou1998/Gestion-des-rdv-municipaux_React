4364a9a9724eb039fc97b3d2a9390d787d022d51
Hadilou1998 <hadilissilame@gmail.com>
2025-02-19 00:06:16 +0100

    Updated index.js back-end/index.js

--- back-end/index.js
+++ back-end/index.js
@@ -25,7 +25,6 @@ if (!process.env.JWT_SECRET) {
 
 // Middlewares
 app.use(cors({
-    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
