00cf956808aa548a6f94c3c63b5321e68e9faeaa
Hadilou1998 <hadilissilame@gmail.com>
2025-02-19 00:43:17 +0100

    Created docker-entrypoint.sh in the folder front

---
+++ front-end/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Lancer Apache en arrière-plan
+httpd -D FOREGROUND -e crit &
+
+# Attendre qu'Apache démarre
+sleep 2
+
+# Afficher le message de succès
+echo "🚀 Frontend lancé avec succès ! Accédez à 👉 http://localhost:3001/"
+
+# Garder le conteneur en vie
+wait
