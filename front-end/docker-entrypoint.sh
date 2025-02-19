2c9e8041003cb22fb1a53b79c34ac2718db87c83
Hadilou1998 <hadilissilame@gmail.com>
2025-02-19 01:30:58 +0100

    Updated docker-entrypoint.sh

--- front-end/docker-entrypoint.sh
+++ front-end/docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "🚀 Frontend lancé avec succès ! Accédez à 👉 http://localhost:3001/"
+STATUS_FILE="/usr/local/apache2/frontend_status"
+
+# Vérifier si le message a déjà été affiché
+if [ ! -f "$STATUS_FILE" ]; then
+    echo "🚀 Frontend lancé avec succès ! Accédez à 👉 http://localhost:3001/"
+    touch "$STATUS_FILE"
+fi
